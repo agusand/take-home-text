4221aeed49b18311c21423e3a94fb9eee7fd0a9c
agusand <amandreacchi@gmail.com>
2022-05-04 00:24:34 -0300

    App.js init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-function App() {
-  return (
-    <div className="App">
+import "./styles/index.css";
 
+export default function App() {
+
+  return (
+    <div className="app">
+      <div className="app__browseFromWrapper">
+      </div>
     </div>
   );
 }
-
-export default App;
