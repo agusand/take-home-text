04b37d25d8bea0b1a16000afb789f81104686f87
agusand <amandreacchi@gmail.com>
2022-05-04 01:08:03 -0300

    Home added

---
+++ src/components/home/UserCard.js
@@ -0,0 +1,73 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+import { useAuthContext } from "../../contexts/AuthContext.js";
+import { useLoadingContext } from "../../contexts/LoadingContext.js";
+
+export default function UserCard() {
+	const { setIsError, setIsSuccess, setPage, userLogged, setUserLogged } =
+		useAuthContext();
+	const { setIsLoading } = useLoadingContext();
+	let navigate = useNavigate();
+
+	useEffect(() => {
+		const getUser = async () => {
+			try {
+				setIsLoading(true);
+				const {
+					data: { user },
+				} = await axios.get("/api/users/logged");
+
+				if (user) {
+					setIsLoading(false);
+					setUserLogged(user);
+				} else {
+					throw new Error("There isn't user logged.");
+				}
+			} catch (error) {
+				setIsLoading(false);
+				console.log(error);
+			}
+		};
+
+		getUser();
+	}, [setIsLoading, setUserLogged]);
+
+	const logoutClickHandle = async () => {
+		try {
+			const { data } = await axios.get("/api/auth/logout");
+
+			if (data.logoutOk) {
+				setPage("logout");
+				setIsSuccess(true);
+				navigate("/auth");
+				setTimeout(() => {
+					setPage("");
+					setIsSuccess(false);
+				}, 2000);
+			} else {
+				setPage("logout");
+				setIsError(true);
+				navigate("/auth");
+				setTimeout(async () => {
+					navigate("/");
+					setPage("");
+					setIsError(false);
+				}, 2000);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	return (
+		<section>
+			<figure>
+				<img alt="user avatar" src={userLogged?.image} />
+			</figure>
+			<p>{`Welcome ${userLogged?.name}!`}</p>
+
+			<button onClick={logoutClickHandle}>Disconnect</button>
+		</section>
+	);
+};
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import UserCard from "../components/home/UserCard.js";
+import Store from "../components/home/Store.js";
+
+export default function Home() {
+    return (
+        <section>
+            <UserCard />
+            <Store />
+        </section>
+    );
+}
