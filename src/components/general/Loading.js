b8dcaab36dc8844dadeda312469e6e47fcbc0258
agusand <amandreacchi@gmail.com>
2022-05-04 19:46:56 -0300

    Loading svg fixed

--- src/components/general/Loading.js
+++ src/components/general/Loading.js
@@ -1,10 +1,21 @@
-import spinner from '../../assets/icons/spinner.svg';
-
 export default function Loading() {
 	return (
 		<div className="loading">
 			<figure className="loading__figure">
-				<img className="loading__image" alt="cargando" src={spinner} />
+				<svg className="loading__image" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 26.3 26.4">
+					<g>
+						<g>
+							<circle fill="currentColor" class="st0" cx="13.8" cy="3.1" r="3.1" />
+							<circle fill="currentColor" class="st0" cx="13.8" cy="24.5" r="1.8" />
+							<circle fill="currentColor" class="st0" cx="6.2" cy="6.2" r="2.8" />
+							<circle fill="currentColor" class="st0" cx="21.4" cy="21.4" r="1.5" />
+							<circle fill="currentColor" class="st0" cx="3.1" cy="13.8" r="2.5" />
+							<circle fill="currentColor" class="st0" cx="24.5" cy="13.8" r="1.2" />
+							<path fill="currentColor" class="st0" d="M4.7,19.8c-0.8,0.8-0.8,2.2,0,3c0.8,0.8,2.2,0.8,3,0c0.8-0.8,0.8-2.2,0-3C6.9,19,5.5,19,4.7,19.8z" />
+							<circle fill="currentColor" class="st0" cx="21.4" cy="6.2" r="0.9" />
+						</g>
+					</g>
+				</svg>
 			</figure>
 		</div>
 	);
