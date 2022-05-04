29c0341fbd5e79c48196b306549efb7260ce2824
agusand <amandreacchi@gmail.com>
2022-05-04 01:06:30 -0300

    React Router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+import Home from "./pages/Home.js";
+
+import AppLayout from "./components/general/AppLayout.js";
+
 import "./styles/index.css";
 
 export default function App() {
 
   return (
-    <div className="app">
-      <div className="app__browseFromWrapper">
-      </div>
-    </div>
+    <BrowserRouter>
+      <AppLayout>
+        <div className="app">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auth" element={<Home />} />
+          </Routes>
+        </div>
+      </AppLayout>
+    </BrowserRouter>
   );
 }
